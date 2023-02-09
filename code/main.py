2a88b820db1ce9591b456c51b4d9363c9477338c
Maxim <dontletthenightmaresbite@gmail.com>
2023-02-09 19:21:59 +0300

    Code commit

--- code/main.py
+++ code/main.py
@@ -1,1 +1,14 @@
+from sqlConnector import *
+db = DataBase()
+print(1)
+db.Connect()
+print(2)
+z=db.Get("select * from Worker")
+a=db.Get("select * from Worker")
+z=db.Get("select * from Worker")
 
+for i in z:
+    print(i)
+
+for i in a:
+    print(i)
