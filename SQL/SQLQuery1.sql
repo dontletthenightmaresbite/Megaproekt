f37b7ff97eabe2921204e2a7f6a113adf722f5d1
Maxim <dontletthenightmaresbite@gmail.com>
2023-02-16 22:02:37 +0300

    create&drop query

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -84,7 +84,7 @@ go
 CREATE TABLE [Worker] (
 	Id int NOT NULL primary key identity,
 	Name nvarchar(50),
-	TeamId int,
+	TeamId int NULL,
 	PhoneNumber nvarchar(50),
 	Post int NOT NULL,
 	IsDeleted bit
@@ -113,21 +113,22 @@ GO
 CREATE TABLE [Teams] (
 	Id int NOT NULL primary key identity,
 	[Name] nvarchar(25),
-	LeaderId int NOT NULL
+	LeaderId int NULL
 
 )
 GO
 CREATE TABLE [Tasks] (
 	Id int NOT NULL primary key identity,
 	[Description] nvarchar(120),
-	TeamId int,
-	WorkerId int,
+	TeamId int NULL,
+	WorkerId int NULL,
 	Deadline date NOT NULL
 
 
 )
 GO
-ALTER TABLE [Worker] WITH CHECK ADD CONSTRAINT [Worker_fk0] FOREIGN KEY ([TeamId]) REFERENCES [Teams]([TeamId])
+
+ALTER TABLE [Worker] WITH CHECK ADD CONSTRAINT [Worker_fk0] FOREIGN KEY ([TeamId]) REFERENCES [Teams]([Id])
 ON UPDATE CASCADE
 GO
 ALTER TABLE [Worker] CHECK CONSTRAINT [Worker_fk0]
@@ -137,7 +138,6 @@ ON UPDATE CASCADE
 GO
 ALTER TABLE [Worker] CHECK CONSTRAINT [Worker_fk1]
 GO
-
 
 ALTER TABLE [Opportunities] WITH CHECK ADD CONSTRAINT [Opportunities_fk0] FOREIGN KEY ([PostId]) REFERENCES [Posts]([Id])
 ON UPDATE CASCADE
@@ -150,22 +150,36 @@ GO
 ALTER TABLE [Opportunities] CHECK CONSTRAINT [Opportunities_fk1]
 GO
 
-
 ALTER TABLE [Teams] WITH CHECK ADD CONSTRAINT [Teams_fk0] FOREIGN KEY ([LeaderId]) REFERENCES [Worker]([Id])
-ON UPDATE CASCADE
+ON UPDATE NO ACTION
 GO
 ALTER TABLE [Teams] CHECK CONSTRAINT [Teams_fk0]
 GO
 
-ALTER TABLE [Tasks] WITH CHECK ADD CONSTRAINT [Tasks_fk0] FOREIGN KEY ([TeamId]) REFERENCES [Teams]([TeamId])
+ALTER TABLE [Tasks] WITH CHECK ADD CONSTRAINT [Tasks_fk0] FOREIGN KEY ([TeamId]) REFERENCES [Teams]([Id])
 ON UPDATE CASCADE
 GO
 ALTER TABLE [Tasks] CHECK CONSTRAINT [Tasks_fk0]
 GO
 ALTER TABLE [Tasks] WITH CHECK ADD CONSTRAINT [Tasks_fk1] FOREIGN KEY ([WorkerId]) REFERENCES [Worker]([Id])
-ON UPDATE CASCADE
+ON UPDATE NO ACTION
 GO
 ALTER TABLE [Tasks] CHECK CONSTRAINT [Tasks_fk1]
 GO
-ALTER TABLE [Tasks] WITH CHECK ADD CONSTRAINT [Tasks_fk2] FOREIGN KEY ([TeamId]) REFERENCES [Teams]([Id])
-alter table Teams alter column LeaderId int null
+
+--#################################################################### DATABASE DROP ####################################################################--
+
+ALTER TABLE [Tasks] DROP CONSTRAINT [Tasks_fk1]
+ALTER TABLE [Tasks] DROP CONSTRAINT [Tasks_fk0]
+ALTER TABLE [Teams] DROP CONSTRAINT [Teams_fk0]
+ALTER TABLE [Opportunities] DROP CONSTRAINT [Opportunities_fk1]
+ALTER TABLE [Opportunities] DROP CONSTRAINT [Opportunities_fk0]
+ALTER TABLE [Worker] DROP CONSTRAINT [Worker_fk1]
+ALTER TABLE [Worker] DROP CONSTRAINT [Worker_fk0]
+
+DROP TABLE [Worker]
+DROP TABLE [Opportunities]
+DROP TABLE [Posts]
+DROP TABLE [ListOfOpportunities]
+DROP TABLE [Teams]
+DROP TABLE [Tasks]
