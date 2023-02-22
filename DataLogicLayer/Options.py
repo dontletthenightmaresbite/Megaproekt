3e0283441002d0f86b7f2487c847cb0b9a2d003c
Maxim <dontletthenightmaresbite@gmail.com>
2023-02-23 00:17:32 +0300

    procedures in options and repos

--- DataLogicLayer/Options.py
+++ DataLogicLayer/Options.py
@@ -1,9 +1,16 @@
 class Options:
     def __init__(self):
+        self.get_team_tasks = 'exec GetTeamTasks'
+        self.get_worker_opportunities = 'exec GetWorkerOpportunities'
+        self.get_full_info_of_worker_by_id = 'exec GetFullInfoOfWorkerById'
+        self.tasks_by_worker_id = 'exec TasksByWorkerId'
         self.insert_worker = 'exec InsertWorker'
         self.insert_team = 'exec InsertTeam'
         self.update_worker_team = 'exec UpdateWorkerTeam'
         self.insert_task = 'exec InsertTask'
         self.update_task_worker = 'exec UpdateTaskWorker'
         self.delete_worker = 'exec DeleteWorker'
+        self.insert_post = 'exec InsertPost'
+        self.insert_opportunity = 'exec InsertOpportunity'
+        self.insert_opportunity_in_list_of_opportunities = 'exec InsertOpportunityInListOfOpportunities'
         
--- DataLogicLayer/Repository.py
+++ DataLogicLayer/Repository.py
@@ -21,9 +21,24 @@ class Repository:
         cursor.execute(query)
         return cursor.fetchall()
 
+    def get_team_tasks(self, id):
+        query = self.options.get_team_tasks + f" {id}"
+        self.get(query)
+
+    def get_worker_opportunities(self, id):
+        query = self.options.get_worker_opportunities + f" {id}"
+        self.get(query)
+
+    def get_full_info_of_worker_by_id(self, id):
+        query = self.options.get_full_info_of_worker_by_id + f" {id}"
+        self.get(query)
+
+    def tasks_by_worker_id(self, id):
+        query = self.options.tasks_by_worker_id + f" {id}"
+        self.get(query)
+
     def insert_worker(self, worker):
-        query = self.options.insert_worker + f" '{worker.name}', {worker.teamId}, \
-                                                '{worker.phoneNumber}', {worker.post}"
+        query = self.options.insert_worker + f" '{worker.name}', {worker.teamId}, '{worker.phoneNumber}', {worker.post}"
         self.do(query)
 
     def insert_team(self, team):
@@ -44,4 +59,16 @@ class Repository:
 
     def delete_worker(self, id):
         query = self.options.delete_worker + f" {id}"
+        self.do(query)
+
+    def insert_post(self, name, salary, timeOfWork):
+        query = self.options.insert_post + f" '{name}', {salary}, {timeOfWork}"
+        self.do(query)
+    
+    def insert_opportunity(self, postId, opportunity):
+        query = self.options.insert_opportunity + f" {postId}, {opportunity}"
+        self.do(query)
+
+    def insert_opportunity_in_list_of_opportunities(self, description):
+        query = self.options.insert_opportunity_in_list_of_opportunities + f" {description}"
         self.do(query)
