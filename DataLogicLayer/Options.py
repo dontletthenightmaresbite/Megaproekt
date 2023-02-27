b6dc8ea5bea8a35f074f97162a37290c19f32bab
_Qnain _ <qnain05@gmail.com>
2023-02-27 16:35:43 +0300

    немного красоты

--- DataLogicLayer/Options.py
+++ DataLogicLayer/Options.py
@@ -1,18 +1,21 @@
 class Options:
     def __init__(self):
+        #Insert
+        self.insert_worker = 'exec InsertWorker'
+        self.insert_team = 'exec InsertTeam'
+        self.insert_task = 'exec InsertTask'
+        self.insert_post = 'exec InsertPost'
+        self.insert_opportunity_in_list_of_opportunities = 'exec InsertOpportunityInListOfOpportunities'
+        self.insert_opportunity = 'exec InsertOpportunity'
+        #Get
         self.get_team_tasks = 'exec GetTeamTasks'
         self.get_worker_opportunities = 'exec GetWorkerOpportunities'
         self.get_full_info_of_worker_by_id = 'exec GetFullInfoOfWorkerById'
         self.tasks_by_worker_id = 'exec TasksByWorkerId'
-        self.insert_worker = 'exec InsertWorker'
-        self.insert_team = 'exec InsertTeam'
+        #Change
         self.update_worker_team = 'exec UpdateWorkerTeam'
-        self.insert_task = 'exec InsertTask'
         self.update_task_worker = 'exec UpdateTaskWorker'
         self.delete_worker = 'exec DeleteWorker'
-        self.insert_post = 'exec InsertPost'
-        self.insert_opportunity = 'exec InsertOpportunity'
-        self.insert_opportunity_in_list_of_opportunities = 'exec InsertOpportunityInListOfOpportunities'
         self.change_team_leader = 'exec ChangeTeamLeader'
         self.change_worker_team = 'exec ChangeWorkerTeam'
         self.delete_from_team = 'exec DeleteFromTeam'
