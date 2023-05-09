55f9e77190dc6dd25df4c4640767859a17fad5a9
Maxim <dontletthenightmaresbite@gmail.com>
2023-05-09 11:54:57 +0300

    bot call

--- botyara/mainBot.py
+++ botyara/mainBot.py
@@ -11,10 +11,19 @@ default = types.InlineKeyboardMarkup().add(newWorker).add(newTeam)
 
 @bot.message_handler(content_types=['text'])
 def any_other(message):
-    bot.send_message(message.from_user.id, message.text,reply_markup=default)
+    bot.send_message(message.from_user.id, message.text, reply_markup=default)
     bot.delete_message(message.chat.id, message.message_id)
     print(message)
 
+
+@bot.callback_query_handler(func=lambda x: True)
+def ananas(call):
+    bot.send_message(call.from_user.id, call.data, reply_markup=default)
+    try:
+        bot.delete_message(call.from_user.id, call.message.message_id)
+    except:
+        True
+    print(call)
 
 @bot.message_handler(commands=['start'])
 def start(message:types.Message):
